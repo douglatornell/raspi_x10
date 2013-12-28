27bb3ce8d61cb4015e2073ecba457d132ad44ce3
Doug Latornell <djl@douglatornell.ca>
2013-12-28 13:35:08 -0800

    Improve upload_schedule script.

--- scripts/upload_schedule.sh
+++ scripts/upload_schedule.sh
@@ -2,14 +2,17 @@
 # Calculate a HEYU schedule based on the state of the away mode device
 # and upload it to the controller
 
+HEYU=/usr/local/bin/heyu
+HEYU_CONFIG=/home/pi/.heyu/x10config
+AWAY_MODE_DEVICE=H16
+
+PYTHON=/usr/bin/python3
+
 RASPI_X10=/home/pi/raspi_x10
 DEVICES=$RASPI_X10/heyu/x10_devices.py
 SPECIAL_DAYS=$RASPI_X10/heyu/special_days.py
-PYTHON=/usr/bin/python3
-HEYU=/usr/local/bin/heyu
-HEYU_CONFIG=/home/pi/.heyu/x10config
 
-AWAY_MODE=$[ $($HEYU -c $HEYU_CONFIG onstate H16) ]
+AWAY_MODE=$[ $($HEYU -c $HEYU_CONFIG onstate $AWAY_MODE_DEVICE) ]
 if [ $AWAY_MODE -eq "0" ]; then
     RULES=$RASPI_X10/heyu/people_home_rules.py
 else

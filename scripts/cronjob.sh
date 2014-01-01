2a890401e0e19b32a2f0c736b7732ce8a1b6a7e3
Doug Latornell <djl@douglatornell.ca>
2014-01-01 13:48:59 -0800

    Delete Heyu config path from cron job; not needed when run as pi user.

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -1,8 +1,7 @@
 HEYU=/usr/local/bin/heyu
-HEYU_CONFIG=/home/pi/.heyu/x10config
 UPLOAD_SCHEDULE=/home/pi/raspi_x10/scripts/upload_schedule.sh
 DOT_HEYU=/home/pi/.heyu
 
-@reboot $HEYU -c $HEYU_CONFIG start
+@reboot $HEYU start
 
 @midnight $UPLOAD_SCHEDULE > $DOT_HEYU/cronjob.out 2> $DOT_HEYU/cronjob.err

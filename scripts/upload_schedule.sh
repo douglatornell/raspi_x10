6c4a5f2dbf4d9be50aeca18f191598135e6d053e
Doug Latornell <djl@douglatornell.ca>
2014-01-02 08:15:17 -0800

    Delete explicit Heyu config use; not reqd when script run as pi user.

--- scripts/upload_schedule.sh
+++ scripts/upload_schedule.sh
@@ -3,7 +3,6 @@
 # and upload it to the controller
 
 HEYU=/usr/local/bin/heyu
-HEYU_CONFIG=/home/pi/.heyu/x10config
 AWAY_MODE_DEVICE=H16
 
 PYTHON=/usr/bin/python3
@@ -12,7 +11,7 @@ RASPI_X10=/home/pi/raspi_x10
 DEVICES=$RASPI_X10/heyu/x10_devices.py
 SPECIAL_DAYS=$RASPI_X10/heyu/special_days.py
 
-AWAY_MODE=$[ $($HEYU -c $HEYU_CONFIG onstate $AWAY_MODE_DEVICE) ]
+AWAY_MODE=$[ $($HEYU onstate $AWAY_MODE_DEVICE) ]
 if [ $AWAY_MODE -eq "0" ]; then
     RULES=$RASPI_X10/heyu/people_home_rules.py
 else
@@ -23,4 +22,4 @@ cd $RASPI_X10 \
 && \
 $PYTHON -m raspi_x10.schedule $DEVICES $RULES $SPECIAL_DAYS \
 && \
-$HEYU -c $HEYU_CONFIG upload
+$HEYU upload
